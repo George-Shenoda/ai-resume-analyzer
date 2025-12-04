77d40c99bf20641cf25010c00ca81099f8dbf419
george-iot <georgeshenoda@protonmail.com>
2025-12-04 14:36:55 +0200

    uploader

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -23,8 +23,8 @@ export default function Home() {
         }
     }, [auth.isAuthenticated]);
     return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+        <Navbar />
         <section className="main-section">
-            <Navbar />
             <div className="page-heading py-16">
                 <h1>Track Your Applications & Resume Ratings</h1>
                 <h2>Review your submissions and check AI-powered feedback.</h2>
