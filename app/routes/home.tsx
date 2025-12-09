2f7d25147e7538178b29d8310c18038b5be80146
george-iot <georgeshenoda@protonmail.com>
2025-12-09 16:48:56 +0200

    finishing

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,41 +1,85 @@
 import Navbar from "~/components/navbar";
-import {resumes} from "../../constants";
 import ResumeCard from "~/components/ResumeCard";
-import {usePuterStore} from "~/lib/puter";
-import {useNavigate} from "react-router";
-import {useEffect} from "react";
+import { usePuterStore } from "~/lib/puter";
+import { Link, useNavigate } from "react-router";
+import { useEffect, useState } from "react";
 import React from "react";
 
 export function meta() {
-  return [
-    { title: "Resumes" },
-    { name: "description", content: "Smart feedback for your dream jop!" },
-  ];
+    return [
+        { title: "Resumes" },
+        { name: "description", content: "Smart feedback for your dream jop!" },
+    ];
 }
 
 export default function Home() {
-    const { auth } = usePuterStore();
+    const { auth, kv } = usePuterStore();
     const navigate = useNavigate();
+    const [resumes, setResumes] = useState<Resume[]>([]);
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
-        if (!auth.isAuthenticated){
-            navigate('/auth?next=/');
+        const loadResumes = async () => {
+            setLoading(true);
+            const storedResumes = (await kv.list("resume:*", true)) as KVItem[];
+            const parsedResumes = storedResumes.map(
+                (item) => JSON.parse(item.value) as Resume,
+            );
+            setResumes(parsedResumes || []);
+            setLoading(false);
+        };
+
+        loadResumes();
+    }, []);
+
+    useEffect(() => {
+        if (!auth.isAuthenticated) {
+            navigate("/auth?next=/");
         }
     }, [auth.isAuthenticated]);
-    return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
-        <Navbar />
-        <section className="main-section">
-            <div className="page-heading py-16">
-                <h1>Track Your Applications & Resume Ratings</h1>
-                <h2>Review your submissions and check AI-powered feedback.</h2>
-            </div>
-            {resumes.length > 0 && (
-                <div className="resumes-section">
-                    {resumes.map(resume => (
-                        <ResumeCard key={resume.id} resume={resume} />
-                    ))}
+
+    return (
+        <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+            <Navbar />
+            <section className="main-section">
+                <div className="page-heading py-16">
+                    <h1>Track Your Applications & Resume Ratings</h1>
+                    {!loading && resumes.length === 0 ? (
+                        <>
+                            <h2>
+                                No Resumes Found. Upload your first resume to
+                                get Feedback!
+                            </h2>
+                            <div className="flex flex-col items-center justify-center mt-10 gap-4">
+                                <Link to="/upload" className="primary-button w-fit text-xl font-semibold">
+                                    Upload Resume
+                                </Link>
+                            </div>
+                        </>
+                    ) : (
+                        <h2>
+                            Review your submissions and check AI-powered
+                            feedback.
+                        </h2>
+                    )}
                 </div>
-            )}
-        </section>
-    </main>;
+                {loading && (
+                    <div className="flex flex-col items-center justify-center">
+                        <img
+                            src="images/resume-scan-2.gif"
+                            alt=""
+                            className="w-[200px]"
+                        />
+                    </div>
+                )}
+                {!loading && resumes.length > 0 && (
+                    <div className="resumes-section">
+                        {resumes.map((resume) => (
+                            <ResumeCard key={resume.id} resume={resume} />
+                        ))}
+                    </div>
+                )}
+            </section>
+        </main>
+    );
 }
